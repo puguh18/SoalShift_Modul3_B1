0c118af4796a0f083a964ad3ff23f4d3f3aa7728
ojoakua-10bit <33028351+ojoakua-10bit@users.noreply.github.com>
2017-10-25 16:27:31 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -7,29 +7,28 @@
 #include <pthread.h>
 
 struct area {
-    int area1[16];
-    int area2[16];
+    int *area1;
+    int *area2;
     int point1;
     int point2;
     char name1[30];
     char name2[30];
 };
 
-int flag == 1;
-
-static void* check(void* arg){
+static void* play(void* arg){
+    struct area *a = (struct area*)arg;
     while(1){
         int j, k;
         printf("%s\'s turn\n", a->name1);
         do {
             printf("Enter the number of your mine (1-4) >>");
             scanf("%d", &j);
-        } while(j < 1 || j > 4)
+        } while(j < 1 || j > 4);
         printf("Select the location of your MINE (1-16)\n");
         for(int i = 0; i < j; i++){
             do{
                 scanf("%d", &k);
-                a->area1[k-1] ? printf("Slot %d already filled\n", k):;
+                if(a->area1[k-1]) printf("Slot %d already filled\n", k);
             } while (a->area1[k-1]);
             a->area1[k-1]++;
         }
@@ -37,17 +36,17 @@ static void* check(void* arg){
         do {
             printf("Enter the number of your mine (1-4) >>");
             scanf("%d", &j);
-        } while(j < 1 || j > 4)
+        } while(j < 1 || j > 4);
         printf("Select the location of your MINE (1-16)\n");
         for(int i = 0; i < j; i++){
             do{
                 scanf("%d", &k);
-                a->area2[k-1] ? printf("Slot %d already filled\n", k):;
+                if(a->area2[k-1]) printf("Slot %d already filled\n", k);
             } while (a->area2[k-1]);
             a->area2[k-1]++;
         }
         printf("%s\'s turn\n", a->name1);
-        printf("Guess 4 of the %s\'s slot that has no MINE", a->name2);
+        printf("Guess 4 of the %s\'s slot that has no MINE\n", a->name2);
         for(int i = 0; i < 4; i++){
             scanf("%d", &k);
             if(a->area2[k-1]){
@@ -60,7 +59,7 @@ static void* check(void* arg){
             }
         }
         printf("%s\'s turn\n", a->name2);
-        printf("Guess 4 of the %s\'s slot that has no MINE", a->name1);
+        printf("Guess 4 of the %s\'s slot that has no MINE\n", a->name1);
         for(int i = 0; i < 4; i++){
             scanf("%d", &k);
             if(a->area1[k-1]){
@@ -77,23 +76,35 @@ static void* check(void* arg){
     return NULL;
 }
 
-static void* play(void* arg){
-    
+static void* check(void* arg){
+    struct area *a = (struct area*)arg;
+    while(1){
+        int k = 0, l = 0;
+        for(int i = 0; i < 16; i++){
+            if(!a->area1[i]) k++;
+            if(!a->area2[i]) l++;
+        }
+        if(a->point1 >= 10 || a->point2 >= 10 || !l || !k) break;
+    }
     return NULL;
 }
 
 int main(){
     pthread_t tid1, tid2;
     struct area a;
-    a.area1 = {0};
-    a.area2 = {0};
+    a.area1 = (int*)calloc(16, sizeof(int));
+    a.area2 = (int*)calloc(16, sizeof(int));
     a.point1 = 0;
     a.point2 = 0;
     printf("Enter player1 name >>");
-    scanf("%s", a.name[0]);
+    scanf("%s", a.name1);
     printf("Enter player2 name >>");
-    scanf("%s", a.name[1]);
-    
-    printf("Stats\n%s: %d\n%s: %d\n", a->name1, a->point1, a->name2, a->point2);
+    scanf("%s", a.name2);
+    pthread_create(&(tid1), NULL, &play, &a);
+    pthread_create(&(tid2), NULL, &check, &a);
+    pthread_join(tid2, NULL);
+    printf("GAME OVER!\n");
+    printf("%s WON THE GAME!!!\n", a.point1 > a.point2 ? a.name1 : a.name2);
+    printf("Stats\n%s: %d\n%s: %d\n", a.name1, a.point1, a.name2, a.point2);
     return 0;
 }
