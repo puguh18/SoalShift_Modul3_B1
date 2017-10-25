4ff4f0b4cf7180d207fa5ee51d60d61852b6e123
ojoakua-10bit <33028351+ojoakua-10bit@users.noreply.github.com>
2017-10-25 13:20:23 +0700

    Create soal1_buyer.c

---
+++ soal1_buyer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+char* types[] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
+
+int main() {
+    key_t key = 1234;
+    int *stock;
+    int shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
+    stock = shmat(shmid, NULL, 0);
+    
+    while(1){
+        int k;
+        printf("WELKAM TO BLEK MEJIK WIPEN STOR\n");
+        printf("~ buyer mode ~\n");
+        printf("1. Check Stock  2. Buy  <and other number to exit>\n");
+        scanf("%d", &k);
+        if(k == 1) check(stock);
+        else if (k == 2) buy(stock);
+        else break;
+    }
+    
+    shmdt(value);
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
+}
